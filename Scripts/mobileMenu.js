98fe36e0def53b7fbe39ae800ea4d56c7e1e413c
Christian Garlen <chadgarval@outlook.com>
2026-07-10 15:53:40 -0400

    Fixed hamburger toggle, change js to toggle between classes instead changing display value of navigation bar

--- Scripts/mobileMenu.js
+++ Scripts/mobileMenu.js
@@ -1,17 +1,9 @@
 // we create a constant for this menu variable, we need to call
 // .nav-list class from css
 const menu = document.querySelector(".nav-list");
-// By default the menu it's usually closed
-let open = false;
 
-// We'll call the function from the html button, when it's closed
-// it will change the status of display between none and flex
+// Once the function is called, It will trigger a toggle method
+// calling .nav-list.show class
 function openMenu() {
-    if (open) {
-        menu.style.display = "none";
-        open = false;
-    } else if (!open) {
-        menu.style.display = "flex";
-        open = true;
-    }
+    menu.classList.toggle("show");
 }
